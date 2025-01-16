4e05613c321fe929398d3fe7a4534df32c015857
pramodvp <pramod_v@hotmail.com>
2025-01-16 16:39:50 -0500

    Updated the AWS region to us-gov-west-1

--- aws_dynamodb_conn.py
+++ aws_dynamodb_conn.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 
-dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
+dynamo_client = boto3.client('dynamodb', region_name='us-gov-west-1')
 
 def get_items():
     return dynamo_client.scan(
